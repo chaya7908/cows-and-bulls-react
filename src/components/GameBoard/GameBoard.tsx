4e1a6811de18639cfc922431f7cd74a3f8914d83
Chaya Stern <cstern@yotpo.com>
2022-08-28 23:04:12 +0300

    feat(game): create gameBoard component

---
+++ src/components/GameBoard/GameBoard.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './GameBoard.scss';
+
+interface GameBoardProps {}
+
+const GameBoard: FC<GameBoardProps> = () => (
+  <div className="GameBoard">
+    GameBoard Component
+  </div>
+);
+
+export default GameBoard;
